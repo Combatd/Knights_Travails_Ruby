467999df48c35120c5b764ffbc721af14213e444
Combatd <combatdstar@gmail.com>
2020-09-18 02:51:30 -0700

    Create class KnightPathFinder, Set KnightPathFinder@root_node

---
+++ lib/knightpathfinder.rb
@@ -0,0 +1,11 @@
+require_relative 'polytreenode'
+
+class KnightPathFinder 
+    # setter for root_node instance variable
+    attr_accessor :root_node
+
+    def initialize(start_pos)
+
+    end
+    
+end
