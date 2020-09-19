a98c58a8d9cd565e271a6a96c8e7078cfe653a07
Combatd <combatdstar@gmail.com>
2020-09-18 22:22:39 -0700

    Define KnightPathFinder#initialize

--- lib/knightpathfinder.rb
+++ lib/knightpathfinder.rb
@@ -2,10 +2,17 @@ require_relative 'polytreenode'
 
 class KnightPathFinder 
     # setter for root_node instance variable
-    attr_accessor :root_node
+    attr_accessor :root_node, :considered_positions
+    # Matrix represents 8 x 8 grid
+    MOVES = [[1,2], [2,1], [-1, 2], [1, -2], [-2, 1], [2, -1], [-2, -1], [-1, -2]]
 
     def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
 
+        # build_move_tree will be needed here
     end
-    
+
+
+
 end
