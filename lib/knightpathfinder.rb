6cb728ffea69b50a1423f20d246edaef1403f39a
Combatd <combatdstar@gmail.com>
2020-09-19 03:09:08 -0700

    Define KnightPathFinder::valid_moves(pos)

--- lib/knightpathfinder.rb
+++ lib/knightpathfinder.rb
@@ -5,7 +5,7 @@ class KnightPathFinder
     attr_accessor :root_node, :considered_positions
     # Matrix represents 8 x 8 grid
     MOVES = [[1,2], [2,1], [-1, 2], [1, -2], [-2, 1], [2, -1], [-2, -1], [-1, -2]]
-
+    
     def initialize(start_pos)
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
@@ -13,6 +13,16 @@ class KnightPathFinder
         # build_move_tree will be needed here
     end
 
+    def self.valid_moves(pos)
+        row = pos.first # first element of pos
+        col = pos.last # second element of pos
 
+        new_moves = MOVES.map do |move|
+            if (row + move.first).between?(0, 7) && (col + move.last).between?(0, 7) # if one of 8 valid moves
+                [row + move.first, col + move.last] # add the move to the current row and column of pos
+            end
+        end
+        new_moves.reject { |ele| ele.nil? } # filter nil values from new_moves and return
+    end
 
 end
